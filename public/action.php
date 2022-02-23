df7b12c4db87ce4bc0bfe291780b067b3c32fdb6
NektoUa <illyabarkov@gmail.com>
2022-02-23 11:55:50 +0200

    add boot strap

--- public/action.php
+++ public/action.php
@@ -1,7 +1,38 @@
 <?php
 //var_dump($_COOKIE);
+session_start();
+//var_dump($_GET);
 $val = $_GET["login"];
 setcookie($val, $val, 0, "/");
-echo "you're registered";
-echo "<p>${val}</p>";
-echo "<a href='index.php'>return</a>";
+$_SESSION[$val] = $val;
+//var_dump($_SESSION);
+require_once('../src/head.php');
+$out = '';
+//if ($_GET['check'] == '') {
+//    echo 'no';
+//} else {
+//    echo 'yes';
+//}
+foreach ($_COOKIE as $key => $value) {
+    if($_COOKIE[$key] == $val){
+        $out = 'You\'re logged and welcome!';
+        break;
+    } else {
+        $out = 'Hello first time. It\'s your guest-book!';
+        break;
+    }
+}
+?>
+<body>
+<div class="position-absolute top-0 start-50 translate-middle-x">
+<h3><?php echo $out; ?></h3>
+    <div class="mb-3">
+        <label for="opinion" class="form-label">Your opinion</label>
+        <textarea class="form-control" id="opinion" rows="3"></textarea>
+        <input type="submit" class="btn btn-primary" value="Add opinion">
+    </div>
+
+<a href='index.php' class="btn btn-primary">return</a>
+</div>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,30 +1,32 @@
 <?php
-
+require_once('../src/head.php');
+//echo $_COOKIE['don'];
+//session_start();
+//echo $_SERVER['HTTP_USER_AGENT'];
+//$_SESSION['test'] = 'singapore';
+//var_dump($_SESSION);
+//echo $_SESSION['test'];
+//session_destroy();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php
-    $a = "cookies and sessions";
-    echo "<title>$a</title>";
-    ?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
 
-<hi>Shalom</hi>
+<body>
+<div class="position-absolute top-50 start-50 translate-middle"">
+<h2 >Shalom</h2>
 
 <form action="action.php" method="GET">
-    <label for="login">enter login</label>
-    <input type="text" name="login" id="login">
-    <input type="submit">
+    <div class="mb-3">
+    <label for="login" class="form-label">enter login</label>
+    <input type="text" name="login" id="login" class="form-control">
+    </div>
+<!--    <div class="mb-3 form-check">-->
+<!--        <input type="checkbox" class="form-check-input" id="checkAgree" name="check">-->
+<!--        <label class="form-check-label" for="checkAgree">I agree</label>-->
+<!--    </div>-->
+    <input type="submit" class="btn btn-primary" value="Enter">
 </form>
+</div>
 <?php
 
-?>
 ?>
 
 </body>
